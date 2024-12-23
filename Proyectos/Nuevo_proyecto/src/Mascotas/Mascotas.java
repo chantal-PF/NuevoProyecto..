a4f72509f3e6ef129c2a629fb3f4c5916be2f66b
chantal-PF <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-12-22 21:45:12 -0500

    Add files via upload

---
+++ Proyectos/Nuevo_proyecto/src/Mascotas/Mascotas.java
@@ -0,0 +1,33 @@
+package Mascotas;
+
+public class Mascotas {
+	
+	
+	public String Nombre;
+	public String Especie;
+	public String Raza;
+	public int Edad;
+	public String Genero;
+	public double Peso;
+	public String ColorDelPelaje;
+	public boolean TieneVacunas;
+	public String TipoAlimentacion;
+	public boolean EsEsterilizado;
+	public String Tamaño;
+	public boolean TieneMicrochip;
+	public String NumeroRegistro;
+	public String Comportamiento; 
+	public boolean EsDomestico;
+    public String NombreDelDueño;
+    public String ElVeterinarioAsignado;
+    public boolean TieneSeguro;
+    public String SuActividadFavorita;
+    public String SuLugarPreferido;
+    public boolean EntrenamientoBasico;
+    public int NumeroDeHermanos;
+    public String FechaDeVista;
+    public String ComidaFavorita;
+    public String HoraFavorita;
+    
+    
+}
